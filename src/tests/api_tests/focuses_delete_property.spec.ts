5cabaa4f8fc755027b98e59f853126547e22ce7f
Facundo Garcia <facundogarcia1993@gmail.com>
2022-01-23 16:58:54 -0300

    clean up delete property tests

---
+++ src/tests/api_tests/focuses_delete_property.spec.ts
@@ -0,0 +1,21 @@
+import { SKILL_DEFINITIONS } from "@core/domain/skill/SKILL_DEFINITIONS"
+import {
+	expect_character_to_have_property_deleted,
+	the_saved_character_has,
+	update_character
+} from "@tests/api_tests/utils"
+
+describe("delete_property focus should", () => {
+	it("remove skill focuses from the character", async () => {
+		the_saved_character_has({ focuses: { [SKILL.code]: [IRRELEVANT_VALUE] } })
+
+		const result = await update_character(["delete_property", PROPERTY])
+
+		expect_character_to_have_property_deleted(PROPERTY)
+		expect(result.statusCode).toBe(200)
+	})
+})
+
+const SKILL = SKILL_DEFINITIONS[1]
+const PROPERTY = `focuses.${SKILL.code}`
+const IRRELEVANT_VALUE = "irrelevant"
---
+++ src/tests/api_tests/spell_delete_property.spec.ts
@@ -0,0 +1,21 @@
+import {
+	expect_character_to_have_property_deleted,
+	the_saved_character_has,
+	update_character
+} from "@tests/api_tests/utils"
+import { TEST_MAGIC_SCHOOLS } from "@tests/web_tests/character_sheet_reducer/utils/collections"
+
+describe("delete_property spell should", () => {
+	it("remove magic school from the character", async () => {
+		the_saved_character_has({ spells: { [SCHOOL.code]: [IRRELEVANT_VALUE] } })
+
+		const result = await update_character(["delete_property", PROPERTY])
+
+		expect_character_to_have_property_deleted(PROPERTY)
+		expect(result.statusCode).toBe(200)
+	})
+})
+
+const SCHOOL = TEST_MAGIC_SCHOOLS[1]
+const PROPERTY = `spells.${SCHOOL.code}`
+const IRRELEVANT_VALUE = "irrelevant"
