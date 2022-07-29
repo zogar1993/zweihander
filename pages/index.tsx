885a878b1467d53bdb78ffbdd5b21d1c902b65d3
Facundo Garcia <facundogarcia1993@gmail.com>
2022-07-29 01:01:22 -0300

    Add changelog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
 export default function Homepage() {
-	return <img src="party.jpg" alt="pc party" />
+	return (
+		<>
+			<img src="party.jpg" alt="pc party" />
+			<h2>Changelog</h2>
+			<ul>
+				<li>- 20220729 - BUGFIX now generalist magic spells show correctly</li>
+			</ul>
+		</>
+	)
 }
