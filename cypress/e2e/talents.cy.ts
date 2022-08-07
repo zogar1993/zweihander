804ca218ed5414dd347e3b89f00eb9d1cb57ad9c
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-08-07 13:56:24 -0300

    make cypress tests independent from contentful client

--- cypress/e2e/talents.cy.ts
+++ cypress/e2e/talents.cy.ts
@@ -1,12 +1,12 @@
-import getTalents from "@core/actions/GetTalents"
+import {Talent} from "@core/domain/types/Talent"
 
 describe("/talents", () => {
 	it("shows all talents", () => {
 		// @ts-ignore ts shenanigans
 		cy.login().then(() => {
 			cy.visit("/talents").then(() =>
-				getTalents().then(talents => {
-					talents.forEach(talent => {
+				cy.fixture("talent.json").then(talents => {
+					(talents as Array<Talent>).forEach(talent => {
 						cy.findByRole("article", {name: talent.name}).findByText(talent.name)
 						cy.findByRole("article", {name: talent.name}).findByText(talent.description)
 						cy.findByRole("article", {name: talent.name}).findByText(talent.effect)
