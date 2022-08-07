9cd2677c949102be4a278b6f01275eae42c20ffe
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-08-07 14:02:08 -0300

    cleanup

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,6 @@ const encrypt = require("cypress-nextjs-auth0/encrypt")
 export default defineConfig({
 	video: false,
 	port: 3001,
-	chromeWebSecurity: false,
 	e2e: {
 		baseUrl: "http://localhost:3000",
 		setupNodeEvents(on, config) {
