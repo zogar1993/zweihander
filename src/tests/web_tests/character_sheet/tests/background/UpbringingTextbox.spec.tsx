9feae802137a793b962dc01bb0b8e8679daae97a
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-08-08 09:33:49 -0300

    refactor upbringing web tests

---
+++ src/tests/web_tests/character_sheet/tests/background/UpbringingTextbox.spec.tsx
@@ -0,0 +1,48 @@
+import {
+	A_USER, ANOTHER_USER,
+	change_combobox_item, given_your_email_is,
+	render_character_sheet, then_textbox_has_a_value_of, then_textbox_is_disabled,
+	update_character_api_was_called_with
+} from "@tests/web_tests/character_sheet/utils/utils"
+import { UPBRINGINGS } from "@web/components/character_sheet/bio/Constants"
+
+const CHARACTER_UPBRINGING = UPBRINGINGS[1]
+const NEW_CHARACTER_UPBRINGING = UPBRINGINGS[2]
+
+describe("Upbringing Combobox should", () => {
+	it("show character sheet upbringing value", async () => {
+		await render_character_sheet({upbringing: CHARACTER_UPBRINGING.code})
+
+		await then_textbox_has_a_value_of("Upbringing", CHARACTER_UPBRINGING.name)
+	})
+	
+	it("send a 'set_value|upbringing' and show updated value on change", async () => {
+		await render_character_sheet({upbringing: CHARACTER_UPBRINGING.code})
+
+		await change_combobox_item("Upbringing", NEW_CHARACTER_UPBRINGING)
+
+		await update_character_api_was_called_with([
+			{
+				action: "set_value",
+				property: "upbringing",
+				value: NEW_CHARACTER_UPBRINGING.code
+			}
+		])
+		await then_textbox_has_a_value_of("Upbringing", NEW_CHARACTER_UPBRINGING.name)
+	})
+
+	it("be disabled if it is not yours", async () => {
+		await given_your_email_is(A_USER)
+		await render_character_sheet({upbringing: CHARACTER_UPBRINGING.code, created_by: ANOTHER_USER})
+
+		await then_textbox_is_disabled("Upbringing")
+		await then_textbox_has_a_value_of("Upbringing", CHARACTER_UPBRINGING.name)
+	})
+
+
+	it("be blank by default", async () => {
+		await render_character_sheet({})
+
+		await then_textbox_has_a_value_of("Upbringing", "")
+	})
+})
