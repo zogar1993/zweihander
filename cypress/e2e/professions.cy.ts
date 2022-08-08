a2c828b9e8d01b327d6fae51176c07166f81ae91
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-08-08 00:14:38 -0300

    make cypress tests faster

--- cypress/e2e/professions.cy.ts
+++ cypress/e2e/professions.cy.ts
@@ -1,22 +1,25 @@
 import {Profession} from "@core/domain/types/Profession"
 
 describe("/professions", () => {
-	(require("../fixtures/profession.json") as Array<Profession>).map(profession =>
-		it(`shows talent "${profession.name}"`, () => {
-			// @ts-ignore ts shenanigans
-			cy.login().then(() => {
-				cy.visit("/professions").then(() => {
 
-					cy.findByRole("article", {name: profession.name}).findByText(profession.name)
-					cy.findByRole("article", {name: profession.name}).findByText(profession.book)
-					cy.findByRole("article", {name: profession.name}).findByText(profession.type)
-					cy.findByRole("article", {name: profession.name}).click()
-					cy.wait(10000).then(() => {
-						cy.findByText(profession.description).then(() => {
-							cy.get("body").type("{esc}")
-						})
-					})
-				})
+	before(() => {
+		// @ts-ignore ts shenanigans
+		cy.login()
+	})
+
+	beforeEach(() => {
+		// @ts-ignore ts shenanigans
+		cy.preserveAuth0CookiesOnce()
+	});
+
+	(require("../fixtures/profession.json") as Array<Profession>).map(profession =>
+		it(`shows profession "${profession.name}"`, () => {
+			cy.visit("/professions").then(() => {
+				cy.findByRole("article", {name: profession.name}).findByText(profession.name)
+				cy.findByRole("article", {name: profession.name}).findByText(profession.book)
+				cy.findByRole("article", {name: profession.name}).findByText(profession.type)
+				cy.findByRole("article", {name: profession.name}).click()
+				cy.findByText(profession.description)
 			})
 		})
 	)
--- cypress/e2e/talents.cy.ts
+++ cypress/e2e/talents.cy.ts
@@ -1,18 +1,23 @@
 import {Talent} from "@core/domain/types/Talent"
 
 describe("/talents", () => {
-	it("shows all talents", () => {
+	before(() => {
 		// @ts-ignore ts shenanigans
-		cy.login().then(() => {
-			cy.visit("/talents").then(() =>
-				cy.fixture("talent.json").then(talents => {
-					(talents as Array<Talent>).forEach(talent => {
-						cy.findByRole("article", {name: talent.name}).findByText(talent.name)
-						cy.findByRole("article", {name: talent.name}).findByText(talent.description)
-						cy.findByRole("article", {name: talent.name}).findByText(talent.effect)
-					})
-				})
-			)
-		})
+		cy.login()
 	})
+
+	beforeEach(() => {
+		// @ts-ignore ts shenanigans
+		cy.preserveAuth0CookiesOnce()
+	});
+
+	(require("../fixtures/talent.json") as Array<Talent>).map(talent =>
+		it(`shows talent ${talent}`, () => {
+			cy.visit("/talents").then(() => {
+				cy.findByRole("article", {name: talent.name}).findByText(talent.name)
+				cy.findByRole("article", {name: talent.name}).findByText(talent.description)
+				cy.findByRole("article", {name: talent.name}).findByText(talent.effect)
+			})
+		})
+	)
 })
