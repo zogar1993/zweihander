2cd58a9b953232cdc5b4c547de427a8c7b29934c
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-09-27 18:47:41 -0300

    disable google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,14 +21,14 @@ const App = ({
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
 			/>
 			<Script strategy="lazyOnload" id="google-analytics-script">
-				{`
+				{/*`
 					window.dataLayer = window.dataLayer || [];
 					function gtag(){dataLayer.push(arguments);}
 					gtag('js', new Date());
 					gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
 					page_path: window.location.pathname,
 					});
-				`}
+				`*/}
 			</Script>
 			<Main ancestries={ancestries} magicSources={magicSources}>
 				<Component {...pageProps} />
